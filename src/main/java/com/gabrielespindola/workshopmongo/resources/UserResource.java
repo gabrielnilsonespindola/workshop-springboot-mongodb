1006c3b307b3cf109aeea23ca034eb5c23da4843
Gabriel Nilson Espindola <gabriel.ne@hotmail.com>
2025-05-08 15:24:03 -0300

    Deleção de usuário com DELETE

--- src/main/java/com/gabrielespindola/workshopmongo/resources/UserResource.java
+++ src/main/java/com/gabrielespindola/workshopmongo/resources/UserResource.java
@@ -6,6 +6,7 @@ import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -41,10 +42,15 @@ public class UserResource {
 		return ResponseEntity.created(uri).build();
 	}
 	
-	@RequestMapping(value ="/users" , method = RequestMethod.GET)
-	public ResponseEntity <UserDTO> findById(@PathVariable String id) {		
-	    User obj = service.findById(id);
-	    return ResponseEntity.ok().body(new UserDTO(obj));   
+	@DeleteMapping("/{id}")
+	public ResponseEntity<Void> delete(@PathVariable String id) {
+	    service.delete(id);
+	    return ResponseEntity.noContent().build();
+	}
 	
+	@RequestMapping(value="/{id}", method=RequestMethod.GET)
+ 	public ResponseEntity<UserDTO> findById(@PathVariable String id) {
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(new UserDTO(obj));
 	}
 }
--- src/main/java/com/gabrielespindola/workshopmongo/services/UserService.java
+++ src/main/java/com/gabrielespindola/workshopmongo/services/UserService.java
@@ -30,6 +30,11 @@ public class UserService {
 		return repo.insert(obj);
 	}
 	
+	public void delete (String id) {
+		findById(id);
+		repo.deleteById(id);
+	}
+	
 	public User fromDTO(UserDTO objDtO) {
 		return new User(objDtO.getId(), objDtO.getName(), objDtO.getEmail());
 	}

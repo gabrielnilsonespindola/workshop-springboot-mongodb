07a3fbcff301b31f22e04cf5c2c3305ce8b8f24a
Gabriel Nilson Espindola <gabriel.ne@hotmail.com>
2025-05-08 15:56:51 -0300

    Atualização de usuário com PUT

--- src/main/java/com/gabrielespindola/workshopmongo/resources/UserResource.java
+++ src/main/java/com/gabrielespindola/workshopmongo/resources/UserResource.java
@@ -53,4 +53,12 @@ public class UserResource {
 		User obj = service.findById(id);
 		return ResponseEntity.ok().body(new UserDTO(obj));
 	}
+	
+	@RequestMapping(value="/{id}", method=RequestMethod.PUT)
+ 	public ResponseEntity<Void> update(@RequestBody UserDTO objDto, @PathVariable String id) {
+		User obj = service.fromDTO(objDto);
+		obj.setId(id);
+		obj = service.update(obj);
+		return ResponseEntity.noContent().build();
+	}
 }
--- src/main/java/com/gabrielespindola/workshopmongo/services/UserService.java
+++ src/main/java/com/gabrielespindola/workshopmongo/services/UserService.java
@@ -35,6 +35,19 @@ public class UserService {
 		repo.deleteById(id);
 	}
 	
+	public User update(User obj) {
+		User newObj = findById(obj.getId());
+		updateData(newObj, obj);
+		return repo.save(newObj);
+	}
+	
+	private void updateData(User newObj, User obj) {
+		newObj.setName(obj.getName());
+		newObj.setEmail(obj.getEmail());
+		
+		
+	}
+
 	public User fromDTO(UserDTO objDtO) {
 		return new User(objDtO.getId(), objDtO.getName(), objDtO.getEmail());
 	}
